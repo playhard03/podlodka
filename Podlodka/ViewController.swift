f6c619e4cf6677692cb637ea0ff401a6006632a7
andre.simagi <63108745+agrmaster@users.noreply.github.com>
2023-03-16 02:07:53 +0500

    Initial Commit

---
+++ Podlodka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podlodka
+//
+//  Created by Андрей Сергеевич on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
